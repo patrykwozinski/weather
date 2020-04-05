151b5a64297f51159068af203d717946d3dfa641
Patryk Woziński <patryk.wozinski@gmail.com>
2020-04-05 13:31:01 +0200

    unit testing in groovy

---
+++ src/test/groovy/com/example/blocks/unit/domain/AggregateRootSpec.groovy
@@ -0,0 +1,55 @@
+package com.example.blocks.unit.domain
+
+import com.example.blocks.domain.AggregateRoot
+import com.example.blocks.domain.Event
+import spock.lang.Specification
+
+import static java.util.UUID.randomUUID
+
+class AggregateRootSpec extends Specification {
+    UUID expectedId = randomUUID()
+    ExampleAggregate aggregate = new ExampleAggregate(expectedId)
+
+    def 'event recorded when action invoked'() {
+        when:
+            aggregate.someAction()
+            Event event = aggregate.pullEvents().get(0)
+        then:
+            event.getId().is(expectedId)
+    }
+
+    def 'no events when pulled second time'() {
+        given:
+            aggregate.someAction()
+        when:
+            aggregate.pullEvents()
+            ArrayList<Event> result = aggregate.pullEvents()
+        then:
+            result.isEmpty()
+    }
+
+    static class ExampleAggregate extends AggregateRoot {
+        UUID expectedId
+
+        ExampleAggregate(UUID id) {
+            this.expectedId = id
+        }
+
+        void someAction() {
+            this.recordThat(new ExampleEvent(expectedId))
+        }
+    }
+
+    static class ExampleEvent implements Event {
+        private final UUID id
+
+        ExampleEvent(UUID id) {
+            this.id = id
+        }
+
+        @Override
+        UUID getId() {
+            return this.id
+        }
+    }
+}
